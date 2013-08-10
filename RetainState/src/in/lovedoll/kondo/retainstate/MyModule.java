60957da96ad5dd82def0b9b007925d4fa6fc64f0
Fumihiko Shiroyama <fumihiko.shiroyama@mixi.co.jp>
2013-08-11 04:16:26 +0900

    Added the class-binding rule for injection

--- RetainState/src/in/lovedoll/kondo/retainstate/MyModule.java
+++ RetainState/src/in/lovedoll/kondo/retainstate/MyModule.java
@@ -1,12 +1,14 @@
 package in.lovedoll.kondo.retainstate;
 
 import proton.inject.DefaultModule;
+import proton.inject.scope.ContextScoped;
 
 public class MyModule extends DefaultModule {
 
     @Override
     protected void configure() {
         super.configure();
+        bind(TextFetcher.class).in(ContextScoped.class);
     }
 
 }
